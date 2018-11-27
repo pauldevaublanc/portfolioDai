e02bacb79bac0630812ebddbf145d8f1026e8cd9
Paul <pauldevaublanc@gmail.com>
2018-11-27 14:03:43 +0100

    Updates

---
+++ precache-manifest.a83c773b6be675adc8ba6f100c8a860e.js
@@ -0,0 +1,358 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0eadd9ea56444c37e572c13d7cc1947",
+    "url": "/portfolioDai/static/media/OXBOW_2.c0eadd9e.png"
+  },
+  {
+    "revision": "1ffca0e8cc9262e8d645",
+    "url": "/portfolioDai/static/css/main.d81247a0.chunk.css"
+  },
+  {
+    "revision": "92a828c2f77557939289",
+    "url": "/portfolioDai/static/js/1.92a828c2.chunk.js"
+  },
+  {
+    "revision": "f22ebe46f978f4e49862",
+    "url": "/portfolioDai/static/js/runtime~main.f22ebe46.js"
+  },
+  {
+    "revision": "214f0bb030068b13e8bad81e35468b54",
+    "url": "/portfolioDai/static/media/WesteindeCaption-Light.214f0bb0.woff2"
+  },
+  {
+    "revision": "f353b86adec91d70f84106ec6a45a2a8",
+    "url": "/portfolioDai/static/media/Lora-Italic.f353b86a.woff2"
+  },
+  {
+    "revision": "21852b638429a645e564541e307aea2d",
+    "url": "/portfolioDai/static/media/Lora-Italic.21852b63.woff"
+  },
+  {
+    "revision": "d7d8f096ca4def8a65e73cd962795c0d",
+    "url": "/portfolioDai/static/media/WesteindeCaption-Light.d7d8f096.woff"
+  },
+  {
+    "revision": "16d67a5fcf14feb246500af618e7e324",
+    "url": "/portfolioDai/static/media/Lora-Italic.16d67a5f.eot"
+  },
+  {
+    "revision": "29f59fe43fdc9addbc5cdcd5df9148cd",
+    "url": "/portfolioDai/static/media/Lora-Italic.29f59fe4.ttf"
+  },
+  {
+    "revision": "0bd2fd2deeb88b5fb3383a667ef28d2c",
+    "url": "/portfolioDai/static/media/NotoSans-Regular.0bd2fd2d.woff2"
+  },
+  {
+    "revision": "272fa3b4396a86874af3a9694999dd1d",
+    "url": "/portfolioDai/static/media/WesteindeCaption-Light.272fa3b4.eot"
+  },
+  {
+    "revision": "3382d0572b7a68ee8ccda333df48f046",
+    "url": "/portfolioDai/static/media/WesteindeCaption-Light.3382d057.ttf"
+  },
+  {
+    "revision": "81a8568cac4d108b27251ab7d3934878",
+    "url": "/portfolioDai/static/media/NotoSans-Regular.81a8568c.woff"
+  },
+  {
+    "revision": "dcf1b9fdf6c045e27adb71ebe2282d8e",
+    "url": "/portfolioDai/static/media/WesteindeCaption-Light.dcf1b9fd.svg"
+  },
+  {
+    "revision": "ef1a0550a7220d77713fcb6925179272",
+    "url": "/portfolioDai/static/media/Lora-Italic.ef1a0550.svg"
+  },
+  {
+    "revision": "446729798b8a7ed8fdc2b5278e29bb4b",
+    "url": "/portfolioDai/static/media/NotoSans-Regular.44672979.eot"
+  },
+  {
+    "revision": "7159805e79fad1e63f1eecf585bdf7c3",
+    "url": "/portfolioDai/static/media/NotoSans-Regular.7159805e.ttf"
+  },
+  {
+    "revision": "90f3878f0253854d69f3f518dd365a75",
+    "url": "/portfolioDai/static/media/NotoSans-Regular.90f3878f.svg"
+  },
+  {
+    "revision": "1ee1846eb03b5555868b9454a0d72f09",
+    "url": "/portfolioDai/static/media/KozGoPro-Bold.1ee1846e.otf"
+  },
+  {
+    "revision": "626bcefb6ef7f5803aef56f16851860a",
+    "url": "/portfolioDai/static/media/NotoSansCJKjp-DemiLight.626bcefb.otf"
+  },
+  {
+    "revision": "b78484fc0c77e797f775f812f73f98d8",
+    "url": "/portfolioDai/static/media/victoryHand.b78484fc.png"
+  },
+  {
+    "revision": "b1657889eb69849c1a147ac59b241c48",
+    "url": "/portfolioDai/static/media/ARCHIVE_1.b1657889.png"
+  },
+  {
+    "revision": "7d5bd5f94b4194f94bf76a8ea0a28ba4",
+    "url": "/portfolioDai/static/media/ARCHIVE_2.7d5bd5f9.png"
+  },
+  {
+    "revision": "57f1c27ca67fb9e2d4ca11a4d48612eb",
+    "url": "/portfolioDai/static/media/BBWALKER_design.57f1c27c.png"
+  },
+  {
+    "revision": "dbbd3322a3cae5c94a7d51444aa5096f",
+    "url": "/portfolioDai/static/media/CHEVIGNON_logo.dbbd3322.png"
+  },
+  {
+    "revision": "1758a573f4d5e298c5fe7f045c63a607",
+    "url": "/portfolioDai/static/media/Caroulus_Valentine.1758a573.png"
+  },
+  {
+    "revision": "a3f9916dd3221d8eedc643f4a828920c",
+    "url": "/portfolioDai/static/media/Covance_leaflet_1.a3f9916d.jpg"
+  },
+  {
+    "revision": "4341b3ade76f30bccad9e81b555170f7",
+    "url": "/portfolioDai/static/media/Covance_leaflet_1.4341b3ad.png"
+  },
+  {
+    "revision": "72cf464816536e7ea042cedbdfae4a4d",
+    "url": "/portfolioDai/static/media/Covance_leaflet_2.72cf4648.jpg"
+  },
+  {
+    "revision": "7be1839ad1bbf46b9125b3d1ba8f2b36",
+    "url": "/portfolioDai/static/media/Covance_leaflet_2.7be1839a.png"
+  },
+  {
+    "revision": "5df21ff7f03eab5cd4e7881d71765cbb",
+    "url": "/portfolioDai/static/media/Covance_leaflet_3.5df21ff7.jpg"
+  },
+  {
+    "revision": "a095c37709ba403971d42cd73e61b9dc",
+    "url": "/portfolioDai/static/media/DKNY.a095c377.png"
+  },
+  {
+    "revision": "184e7755a9a8adabdada473e8680b928",
+    "url": "/portfolioDai/static/media/DKNY_1.184e7755.png"
+  },
+  {
+    "revision": "ab85278f9a606496f880c9e090594c2c",
+    "url": "/portfolioDai/static/media/DKNY_logo.ab85278f.png"
+  },
+  {
+    "revision": "a9ecb91962d0c3dd448b636ad7ca47d3",
+    "url": "/portfolioDai/static/media/Daigondo_resume.a9ecb919.png"
+  },
+  {
+    "revision": "079988ca92599d21e2f32362127975a1",
+    "url": "/portfolioDai/static/media/IKKS-SummerTrip_02.079988ca.jpg"
+  },
+  {
+    "revision": "c733b34a34f81c13144d7bc56a6f02b3",
+    "url": "/portfolioDai/static/media/KEED_1.c733b34a.png"
+  },
+  {
+    "revision": "7516f626a4f0544d04634e3f8365c5b7",
+    "url": "/portfolioDai/static/media/KEED_2.7516f626.png"
+  },
+  {
+    "revision": "2469f5bf0d03215fbcc2f2392a7fe2fe",
+    "url": "/portfolioDai/static/media/KEED_3.2469f5bf.png"
+  },
+  {
+    "revision": "9a34827938c7057b79c03f5672fe4b8c",
+    "url": "/portfolioDai/static/media/KEED_logo.9a348279.png"
+  },
+  {
+    "revision": "59e5352c4be01864550cd0db5f3136cf",
+    "url": "/portfolioDai/static/media/Mokatine_packaging_1.59e5352c.png"
+  },
+  {
+    "revision": "b97a750d5b36f171d2e22b7bb57c8ccb",
+    "url": "/portfolioDai/static/media/Mokatine_packaging_2.b97a750d.png"
+  },
+  {
+    "revision": "1b9e7ca261e8ed138ae4b3ee2b944694",
+    "url": "/portfolioDai/static/media/OXBOW_1.1b9e7ca2.png"
+  },
+  {
+    "revision": "1ffca0e8cc9262e8d645",
+    "url": "/portfolioDai/static/js/main.1ffca0e8.chunk.js"
+  },
+  {
+    "revision": "293f8ea07f36665d054c86599416447f",
+    "url": "/portfolioDai/static/media/OXBOW_3.293f8ea0.png"
+  },
+  {
+    "revision": "ad7852c57a10837b7b2291cc21ae5c9c",
+    "url": "/portfolioDai/static/media/OXBOW_4.ad7852c5.png"
+  },
+  {
+    "revision": "539bfcc998783a99396a4441eaabe1df",
+    "url": "/portfolioDai/static/media/TOKYOITER.539bfcc9.png"
+  },
+  {
+    "revision": "2888b5c142f3c0fa7f955633d38d115a",
+    "url": "/portfolioDai/static/media/VEDETT_1.2888b5c1.png"
+  },
+  {
+    "revision": "e9a119c0b82a9a277de8f237a4ab71b4",
+    "url": "/portfolioDai/static/media/VEDETT_2.e9a119c0.png"
+  },
+  {
+    "revision": "7bf2f3a82f3869b5532770da23741ea4",
+    "url": "/portfolioDai/static/media/Warsteiner_valentine.7bf2f3a8.png"
+  },
+  {
+    "revision": "8198b91023ac9f73eb2647ada6a00e1b",
+    "url": "/portfolioDai/static/media/chevignon_1.8198b910.jpg"
+  },
+  {
+    "revision": "37b84d254c8f5b617f657b3a3e03e63c",
+    "url": "/portfolioDai/static/media/chevignon_2.37b84d25.jpg"
+  },
+  {
+    "revision": "9c609c2725601089125fa87ebd1e875b",
+    "url": "/portfolioDai/static/media/chevignon_3.9c609c27.png"
+  },
+  {
+    "revision": "55aef5fa36ecab3b4d118efbafd71e13",
+    "url": "/portfolioDai/static/media/chevignon_4.55aef5fa.jpg"
+  },
+  {
+    "revision": "8426ee92ff28ddc82573e863fb14a0f8",
+    "url": "/portfolioDai/static/media/chevignon_5.8426ee92.jpg"
+  },
+  {
+    "revision": "c68b0adc8c4ddf7ab9bbee4daacf7eab",
+    "url": "/portfolioDai/static/media/img_CHEVIGNON_1.c68b0adc.png"
+  },
+  {
+    "revision": "ec0b134000f67610fb7fafe7a2b937be",
+    "url": "/portfolioDai/static/media/img_CHEVIGNON_2.ec0b1340.png"
+  },
+  {
+    "revision": "6d4dcf0eca5770316cd386f8a713f4c8",
+    "url": "/portfolioDai/static/media/img_CHEVIGNON_3.6d4dcf0e.jpg"
+  },
+  {
+    "revision": "9ecad5906097c598da69d1c05d2f0cc4",
+    "url": "/portfolioDai/static/media/img_CHEVIGNON_4.9ecad590.jpg"
+  },
+  {
+    "revision": "4cde2e7b2f1872e2ebe60087f1918257",
+    "url": "/portfolioDai/static/media/img_CHEVIGNON_5.4cde2e7b.jpg"
+  },
+  {
+    "revision": "3a794439352039bc9de00ee8621af985",
+    "url": "/portfolioDai/static/media/img_CHEVIGNON_6.3a794439.jpg"
+  },
+  {
+    "revision": "6c6123fc668063e125d7eacaca290bd9",
+    "url": "/portfolioDai/static/media/img_CHEVIGNON_7.6c6123fc.jpg"
+  },
+  {
+    "revision": "9a0d72588586e6c04a8cdc5a18fb0501",
+    "url": "/portfolioDai/static/media/img_LEVIS_1.9a0d7258.jpg"
+  },
+  {
+    "revision": "5bc8bd2a47316b8082046391eccf6076",
+    "url": "/portfolioDai/static/media/img_LEVIS_10.5bc8bd2a.jpg"
+  },
+  {
+    "revision": "8e5181a4ee2164a8421bde146e62fae7",
+    "url": "/portfolioDai/static/media/img_LEVIS_11.8e5181a4.jpg"
+  },
+  {
+    "revision": "f46471d682513c2c37d3df806a5f2275",
+    "url": "/portfolioDai/static/media/img_LEVIS_2.f46471d6.jpg"
+  },
+  {
+    "revision": "19f49c3ffccd99fa6a7caa9e289012c9",
+    "url": "/portfolioDai/static/media/img_LEVIS_3.19f49c3f.jpg"
+  },
+  {
+    "revision": "1cdbe1d2f7bc6bcf84577e8d712f59cf",
+    "url": "/portfolioDai/static/media/img_LEVIS_4.1cdbe1d2.jpg"
+  },
+  {
+    "revision": "9c111a557fe8fc9ea6a3aa436eecfa28",
+    "url": "/portfolioDai/static/media/img_LEVIS_5.9c111a55.jpg"
+  },
+  {
+    "revision": "2682daee74f0b82562dc45da027950f9",
+    "url": "/portfolioDai/static/media/img_LEVIS_6.2682daee.jpg"
+  },
+  {
+    "revision": "ecec4b94894dcd0e073252cfa5379ac8",
+    "url": "/portfolioDai/static/media/img_LEVIS_7.ecec4b94.jpg"
+  },
+  {
+    "revision": "b73b704d7b310105bc7dbeb72a921cef",
+    "url": "/portfolioDai/static/media/img_LEVIS_8.b73b704d.jpg"
+  },
+  {
+    "revision": "177603eee18d4d0cfe99f5407466a448",
+    "url": "/portfolioDai/static/media/img_LEVIS_9.177603ee.jpg"
+  },
+  {
+    "revision": "91db08c650279cae43b99c0d7d9f2e44",
+    "url": "/portfolioDai/static/media/interest1.91db08c6.png"
+  },
+  {
+    "revision": "efc62759490e369528dc060dad12d042",
+    "url": "/portfolioDai/static/media/interest2.efc62759.png"
+  },
+  {
+    "revision": "c39041007c71b606b7d5a52bf6d86653",
+    "url": "/portfolioDai/static/media/interest3.c3904100.png"
+  },
+  {
+    "revision": "2272873684e61837ae148ff21f3ed644",
+    "url": "/portfolioDai/static/media/interest4.22728736.png"
+  },
+  {
+    "revision": "99547c776e35e8620c43823b71425bd5",
+    "url": "/portfolioDai/static/media/levis_1.99547c77.png"
+  },
+  {
+    "revision": "19d396b94ab879d566d6de8d2ad5d6d6",
+    "url": "/portfolioDai/static/media/levis_2.19d396b9.jpg"
+  },
+  {
+    "revision": "f585a55355901b173505eb856ee96b77",
+    "url": "/portfolioDai/static/media/levis_3.f585a553.png"
+  },
+  {
+    "revision": "3bed020b6584535893a7e046818b494f",
+    "url": "/portfolioDai/static/media/levis_4.3bed020b.png"
+  },
+  {
+    "revision": "51ad3183def5cbe61123d2f0cfb44f04",
+    "url": "/portfolioDai/static/media/levis_5.51ad3183.png"
+  },
+  {
+    "revision": "a43c59e6c97bd2fd7c1fa92818b5eb36",
+    "url": "/portfolioDai/static/media/levis_6.a43c59e6.png"
+  },
+  {
+    "revision": "98337c79f7dbf420fcf576dfaf1fc80f",
+    "url": "/portfolioDai/static/media/oxbow-1.98337c79.png"
+  },
+  {
+    "revision": "0efd8ef2d174bed9bd1dbf448f5257cc",
+    "url": "/portfolioDai/static/media/oxbow-2.0efd8ef2.png"
+  },
+  {
+    "revision": "38598a38751beb60c45d2269806ad972",
+    "url": "/portfolioDai/static/media/vedett.38598a38.png"
+  },
+  {
+    "revision": "f5b5720adf320a127224cf930e5266fc",
+    "url": "/portfolioDai/static/media/werkstatt.f5b5720a.png"
+  },
+  {
+    "revision": "926a466f93409fd3c18b30784acb6591",
+    "url": "/portfolioDai/index.html"
+  }
+];
